50716692951b7944897222bf993da6ad45c8995f
加缪 <jiamiu.hzy@alibaba-inc.com>
2014-09-22 20:59:17 +0800

    add rbac example to module twenty-admin

---
+++ modules/twenty-admin/index.js
@@ -0,0 +1,40 @@
+var q = require('q')
+
+var admin = {
+  deps : ['rbac', 'model'],
+  acl : {
+    routes :{
+      'GET /post' : ['loggedIn'],
+      'GET /post/:id' : ['owner']
+    },
+    roles : {
+      loggedIn : function(req){
+        return req.session.user && req.session.user.id
+      },
+      anonymous : function(req){
+        return !req.session.user || !req.session.user.id
+      },
+      owner : function(req){
+        var tmp = req.path.slice(1).split("/"),
+          modelName = tmp[0],
+          id = tmp[1]
+
+        if( !modelName || !id || !admin.dep.model.models[modelName]){
+          return false
+        }
+
+        return q.Promise(function( resolve, reject){
+          admin.dep.model[modelName].findOne(id).then(function(model){
+            if( model && model.user && model.user.id == req.session.user.id ){
+              resolve()
+            }else{
+              reject()
+            }
+          }).fail(reject)
+        })
+      }
+    }
+  }
+}
+
+module.exports = admin
