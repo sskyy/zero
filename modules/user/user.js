4814395e1f8a3b929cb02f4ad1d013607b8ccc32
加缪 <jiamiu.hzy@alibaba-inc.com>
2014-09-22 20:57:22 +0800

    init session when start

--- modules/user/user.js
+++ modules/user/user.js
@@ -5,7 +5,16 @@ module.exports = {
   models : require('./models'),
   listen : require('./listen')(config),
   //this will allow app global config overwrite
-  config : config
+  config : config,
+  route : {
+    "*" : {
+      "function" : function initSession(req,res,next){
+        req.session.user = {}
+        next()
+      },
+      "order" : {first:true}
+    }
+  }
 }
 
 
