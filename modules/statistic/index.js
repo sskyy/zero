1809066f35e0b7935b60458e62969695613d9e84
加缪 <jiamiu.hzy@alibaba-inc.com>
2014-09-23 17:16:28 +0800

    module statistic complete

--- modules/statistic/index.js
+++ modules/statistic/index.js
@@ -1,79 +1,81 @@
-/**
- * Created by jiamiu on 14-9-18.
- */
+var _ = require('lodash'),
+  moment = moment = require('moment')
+
 module.exports ={
-//  deps : ['model','rest','bus','respond'],
-//  models : require("./models"),
-//  listen : {},
-//  route : {},
-//  strategy : {
-//    daily : function(){
-//
-//    },
-//    feed : function(){
-//
-//    }
-//  },
-//  expand : function(module){
-//    var root = this
-//    if( module.strategy ){
-//      _.extend( root.strategy, module.strategy)
-//    }
-//
-//    if( module.statistics ){
-//      _.forEach( module.statistics,function( handler, event){
-//        if( event[0] == '/' ){
-//          root.route[event] ? root.route[event].push(handler) : (root.route[event]=[handler])
-//        }else{
-//          root.listen[event] ? root.listen[event].push(handler) : (root.listen[event]=[handler])
-//        }
-//      })
-//    }
-//  },
-//  bootstrap : function(){
-//    this.listen = this.standardListeners( this.listen )
-//    this.route = this.standardRoutes( this.routes )
-//    this.dep.bus.expand(this)
-//  },
-//  standardListeners : function( listen ){
-//    var root = this
-//    return _.mapValues( listen, function( handlers ){
-//      return function(){
-//        var bus = this
-//        var argv = _.toArray(arguments).slice(0)
-//        _.forEach(handlers, function(handler){
-//          if(_.isString(handler)&&root.strategy[handler]){
-//            //use predefined handler
-//            root.strategy[handler].apply(bus,argv)
-//          }else if(_.isFunction(handler)){
-//            //use custom handler
-//            handler.apply(bus,argv)
-//          }else{
-//            ZERO.warn('statistic','unknown statistic handler')
-//          }
-//        })
-//      }
-//    })
-//  },
-//  standardRoutes : function( listen ){
-//    var root = this
-//    return _.mapValues( listen, function( handlers ){
-//      return function(){
-//        var bus = this
-//        var argv = _.toArray(arguments).slice(0)
-//        _.forEach(handlers, function(handler){
-//          //TODO support verbose route handler
-//          if(_.isString(handler)&&root.strategy[handler]){
-//            //use predefined handler
-//            root.strategy[handler].apply(bus,argv)
-//          }else if(_.isFunction(handler)){
-//            //use custom handler
-//            handler.apply(bus,argv)
-//          }else{
-//            ZERO.warn('statistic','unknown statistic handler')
-//          }
-//        })
-//      }
-//    })
-//  }
+  deps : ['model','rest','bus','respond','request'],
+  models : require("./models"),
+  listen : {},
+  route : {},
+  strategy : require('./strategy'),
+  expand : function(module){
+    var root = this
+    if( module.statistics.strategy ){
+      _.merge( root.strategy, module.statistics.strategy)
+    }
+
+    if( module.statistics.log ){
+      _.forEach( module.statistics.log,function( handler, event){
+        if( /^[GPD\/]/.test(event) ){
+          root.route[event] ? root.route[event].push(handler) : (root.route[event]=[handler])
+        }else{
+          root.listen[event] ? root.listen[event].push(handler) : (root.listen[event]=[handler])
+        }
+      })
+    }
+  },
+  bootstrap : {
+    "function" : function(){
+      this.listen = this.standardListeners( this.listen )
+      this.route = this.standardRoutes( this.route )
+      this.dep.bus.expand(this)
+      this.dep.request.expand(this)
+    },
+    "order" : {"before":"request.bootstrap"}
+  },
+  standardListeners : function( listen ){
+    var root = this
+    return _.mapValues( listen, function( handlers, event ){
+      return function(){
+        var bus = this
+        var argv = _.toArray(arguments).slice(0)
+        _.forEach(handlers, function(handler){
+          if(_.isString(handler)&&root.strategy.listener[handler]){
+            //use predefined handler
+            root.strategy.listener[handler].apply(bus,[event].concat(argv))
+          }else{
+            ZERO.warn('statistic','unknown statistic handler')
+          }
+        })
+      }
+    })
+  },
+  standardRoutes : function( listen ){
+    var root = this
+    return _.mapValues( listen, function( handlers, url ){
+      return {
+        "function":function(req, res, next ){
+          ZERO.mlog("statistic","log",url)
+
+          applyNext(0)
+
+          function applyNext( n ){
+            if( !handlers[n] ){
+              return next()
+            }
+
+            var applyResult = root.strategy.route[handlers[n]].call( root, url, req )
+            if( applyResult && applyResult.then ){
+              applyResult.fin(function(){
+                applyNext(++n)
+              })
+            }else{
+              applyNext(++n)
+            }
+          }
+
+        },
+        "order" : {first:true}
+      }
+    })
+  }
 }
