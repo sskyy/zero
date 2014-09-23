23fd65d3e95c732249f1f36a42ad764b64abf3bd
加缪 <jiamiu.hzy@alibaba-inc.com>
2014-09-23 17:21:35 +0800

    reformat dev module code

--- system/modules/dev/index.js
+++ system/modules/dev/index.js
@@ -2,65 +2,69 @@ var q = require('q'),
   path = require('path'),
   _ = require('lodash')
 
-function duplicate( str, num ){
-  return (new Array(num+1)).join(str)
+function duplicate(str, num) {
+  return (new Array(num + 1)).join(str)
 }
 
-function cleanCircular( object, preCached ){
+function cleanCircular(object, preCached) {
   preCached = preCached || {}
   var cached = _.values(preCached),
-    cachedNamespace = _.keys( preCached)
+    cachedNamespace = _.keys(preCached)
 
-  ;(function _cleanCircular(obj, namespace){
-    _.forEach( obj, function( v, k){
-      if( !obj.hasOwnProperty(k)) return
-      var i = cached.indexOf( v)
+    ;
+  (function _cleanCircular(obj, namespace) {
+    _.forEach(obj, function (v, k) {
+      if (!obj.hasOwnProperty(k)) return
+      var i = cached.indexOf(v)
 
-      if( v === obj ){
+      if (v === obj) {
         obj[k] = "{circular reference of root object}"
-      }else if( i !== -1 ){
-        obj[k] = "{circular reference of "+cachedNamespace[i]+"}"
-      }else{
-        if( _.isArray(v) || _.isObject(v)){
+      } else if (i !== -1) {
+        obj[k] = "{circular reference of " + cachedNamespace[i] + "}"
+      } else {
+        if (_.isArray(v) || _.isObject(v)) {
           cached.push(v)
           namespace.push(k)
-          cachedNamespace.push(namespace.join('.') )
+          cachedNamespace.push(namespace.join('.'))
           _cleanCircular(v, namespace)
         }
       }
     })
-  }(object,[]))
+  }(object, []))
 
   return object
 }
 
-module.exports =  {
-  deps : ['statics','request'],
-  route : {
-    "/dev/simulate" : function (req, res){
+module.exports = {
+  deps: ['statics', 'request'],
+  route: {
+    "/dev/simulate": {
+      "function": function (req, res) {
 
-      var url = req.body.url,
-        method = req.body.method,
-        data = req.body.data
+        var url = req.body.url,
+          method = req.body.method,
+          data = req.body.data
 
-      req.body = data
+        req.body = data
 
-      req.bus.fire('request.mock',{url:url,method:method,req:req,res:res})
+        req.bus.fire('request.mock', {url: url, method: method, req: req, res: res})
 
-      //all done
-      console.log(req.bus['$$results'])
-      req.bus.then(function(){
-        ZERO.mlog('dev','mock',url,'done')
-        try{
-          res.json(cleanCircular( req.bus.$$traceRoot))
-        }catch(e){
-          res.status(500).end()
-        }
-      })
+        //all done
+        console.log(req.bus['$$results'])
+        req.bus.then(function () {
+          ZERO.mlog('dev', 'mock', url, 'done')
+          try {
+            res.json(cleanCircular(req.bus.$$traceRoot))
+          } catch (e) {
+            res.status(500).end()
+          }
+        })
 
+      },
+      order: {before: "respond.respondHandler"}
     }
   },
-  statics : {
+  statics: {
     "/dev": path.join(__dirname, './public')
   }
 }
