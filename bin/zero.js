9e3f789603f8c9b071a0ca5a39a011da2b0e0298
加缪 <jiamiu.hzy@alibaba-inc.com>
2014-10-17 00:38:43 +0800

    make zero standard npm module

--- bin/zero.js
+++ bin/zero.js
@@ -1,12 +1,5 @@
 #!/usr/bin/env node
 
-var fs = require('fs'),
-  fsExtra  = require('fs-extra'),
-  async = require('async'),
-  _  = require('lodash'),
-  path = require('path'),
-  tosource = require('tosource')
-
 var program = require('commander');
 
 program
--- system/modules/session.js
+++
@@ -1,9 +0,0 @@
-var session = require('express-session')
-
-
-module.exports = {
-  sessionHandler : session({secret: 'zero meant to be rise from the bottom'}),
-  init : function(){
-    APP.use( this.sessionHandler)
-  }
-}
